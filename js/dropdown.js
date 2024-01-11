e2185e851ff45f579edca4ff966a0f6ca24eb914
Ivan Eliseev <zeberg171@gmail.com>
2024-01-11 14:36:52 +0300

    fix: popup

---
+++ js/dropdown.js
@@ -0,0 +1,27 @@
+const dropList = document.querySelectorAll('.header__list-item');
+const dropLink = document.querySelectorAll('#dropdown');
+
+
+document.addEventListener('click', (e) => {
+  const isClickInsideDropList = Array.from(dropList).some((list) => list.contains(e.target));
+  const isClickInsideDropLink = Array.from(dropLink).some((link) => link.contains(e.target));
+
+  if (!isClickInsideDropList && !isClickInsideDropLink) {
+    dropLink.forEach((link) => {
+      link.classList.remove('show');
+    });
+  }
+});
+
+dropList.forEach((list, index) => {
+  list.addEventListener('click', () => {
+    dropLink.forEach((link, linkIndex) => {
+      if (index === linkIndex) {
+        link.classList.toggle('show');
+      } else {
+        link.classList.remove('show');
+      }
+    });
+    e.stopPropagation();
+  });
+});
--- js/popup.js
+++ js/popup.js
@@ -1,35 +1,58 @@
-const popupCallback = document.querySelector('.popup-callback')
-const popupMessage = document.querySelector('.popup-message')
-const popupClose = document.querySelector('.popup__close')
+const popupCallback = document.querySelector('.popup-callback');
+const popupMessage = document.querySelector('.popup-message');
+const popupClose = document.querySelectorAll('.popup__close');
+const popupBtnSubmit = document.querySelectorAll('.popup-btn');
+const callbackButton = document.querySelectorAll('.btn__figurative-connection');
+const messageButton = document.querySelectorAll('.btn__write-to');
+const popup = document.querySelectorAll('.popup')
 
-const popupBtnSubmit = document.querySelectorAll('.popup-btn')
+popup.forEach((popup) => {
+  popup.addEventListener('click', (e) => {
+    if (e.target === popup) {
+      closeModal(popup)
+    }
+  })
+})
 
-const callbackButton = document.querySelectorAll('.btn__figurative-connection')
-const messageButton = document.querySelectorAll('.btn__write-to')
+function closeModal(popup) {
+  popup.classList.remove('popup_open')
+}
+
+document.addEventListener('keydown', (e) => {
+  if (e.code === 'Escape') {
+    popup.forEach((popup) => {
+      if (popup.classList.contains('popup_open')) {
+        closeModal(popup);
+      }
+    });
+  }
+});
 
 callbackButton.forEach((btn) => {
   btn.addEventListener('click', (e) => {
     e.preventDefault();
-    popupCallback.classList.add('popup_open')
-  })
+    popupCallback.classList.add('popup_open');
+  });
 });
 
 messageButton.forEach((btn) => {
   btn.addEventListener('click', (e) => {
     e.preventDefault();
-    popupMessage.classList.add('popup_open')
-  })
+    popupMessage.classList.add('popup_open');
+  });
 });
 
 popupBtnSubmit.forEach((btn) => {
   btn.addEventListener('click', (e) => {
-    e.preventDefault()
-    popupMessage.classList.remove('popup_open')
-    popupCallback.classList.remove('popup_open')
-  })
-})
+    e.preventDefault();
+    popupMessage.classList.remove('popup_open');
+    popupCallback.classList.remove('popup_open');
+  });
+});
 
-popupClose.addEventListener('click', () => {
-  popupMessage.classList.remove('popup_open')
-  popupCallback.classList.remove('popup_open')
-})
+popupClose.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    popupMessage.classList.remove('popup_open');
+    popupCallback.classList.remove('popup_open');
+  });
+});
